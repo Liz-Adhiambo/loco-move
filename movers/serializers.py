9699a1c254b93deaef15514806c3d7cc97606961
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2023-06-08 22:44:44 +0300

    Added profiles

--- movers/serializers.py
+++ movers/serializers.py
@@ -31,3 +31,8 @@ class DriverSerializer(serializers.ModelSerializer):
     class Meta:
         model = Driver
         fields = ('__all__')
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('__all__')
--- movers/urls.py
+++ movers/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
 
 path('user/login', user_login_view, name='clientlogin'),
 path('driver/signup', driver_signup_view, name='driver_signup_view'),
+path('profiles/',profile_list),
+path('profiles/<int:pk>/',profile_detail),
 
 ]
